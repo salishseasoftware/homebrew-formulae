4af146603dd6de5ab433ac1d829be154f1c1481e
Jeff Johnston <hishma@users.noreply.github.com>
2021-03-03 19:57:15 -0800

    Update formula

--- Formula/random-word.rb
+++ Formula/random-word.rb
@@ -1,15 +1,11 @@
 class RandomWord < Formula
-  homepage "https://github.com/salishseasoftware/random-word"
-  head "https://github.com/salishseasoftware/random-word.git"
-  url "https://github.com/salishseasoftware/random-word/archive/0.1.2.tar.gz"
-  sha256 "6470479b2b6c8732bb66d95fa9e98e7d4fb6d31b"
+    homepage "https://github.com/salishseasoftware/random-word"
+    head "https://github.com/salishseasoftware/random-word.git"
+    url "https://github.com/salishseasoftware/random-word/archive/0.1.2.tar.gz"
+    sha256 "6470479b2b6c8732bb66d95fa9e98e7d4fb6d31b"
   
-  def install
-    if build.head?
-      bin.mkpath
-      system "make", "install", "PREFIX=#{ bin }"
-    else
-      bin.install "set-simulator-location"
+    def install
+        bin.mkpath
+        system "make", "install", "PREFIX=#{ bin }"
     end
-  end
 end
