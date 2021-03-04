b8f6f85fc2061f94b741ec68d917417727beda71
Jeff Johnston <hishma@users.noreply.github.com>
2021-03-03 20:00:11 -0800

    Update formula

--- Formula/random-word.rb
+++ Formula/random-word.rb
@@ -1,11 +1,20 @@
+
+
 class RandomWord < Formula
+    desc "Yet another random word generator"
     homepage "https://github.com/salishseasoftware/random-word"
+    url "https://github.com/salishseasoftware/random-word.git",
+      :tag => "0.1.2", :revision => "6470479b2b6c8732bb66d95fa9e98e7d4fb6d31b"
     head "https://github.com/salishseasoftware/random-word.git"
-    url "https://github.com/salishseasoftware/random-word/archive/0.1.2.tar.gz"
-    sha256 "6470479b2b6c8732bb66d95fa9e98e7d4fb6d31b"
-  
+    license "MIT"
+    
+    depends_on :xcode => ["12.0", :build]
+    
     def install
-        bin.mkpath
-        system "make", "install", "PREFIX=#{ bin }"
+        system "make", "install", "prefix=#{bin}"
+    end
+    
+    test do
+        system "#{bin}/onx-tool" "--count=3"
     end
 end
