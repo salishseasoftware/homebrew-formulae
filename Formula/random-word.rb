586f5d78d595fd7816613d007afa349d8a351d0c
Jeff Johnston <hishma@users.noreply.github.com>
2021-03-03 19:44:58 -0800

    Update formula

--- Formula/random-word.rb
+++ Formula/random-word.rb
@@ -1,15 +1,15 @@
 class RandomWord < Formula
-  desc "Yet another random word generator"
   homepage "https://github.com/salishseasoftware/random-word"
+  head "https://github.com/salishseasoftware/random-word.git"
   url "https://github.com/salishseasoftware/random-word/archive/0.1.2.tar.gz"
-  sha256 "6ed8c0254196e8b256df71e1a951ba65287225f1769632df4b5da5b5de28e633"
-  license "MIT"
-
+  sha256 "6470479b2b6c8732bb66d95fa9e98e7d4fb6d31b"
+  
   def install
-    system "make", "install", "prefix=#{prefix}"
-  end
-
-  test do
-    system "#{bin}/onx-tool" "--count=3"
+    if build.head?
+      bin.mkpath
+      system "make", "install", "PREFIX=#{ bin }"
+    else
+      bin.install "set-simulator-location"
+    end
   end
 end
